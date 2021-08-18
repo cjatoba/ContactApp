cf7c709dc2650316fad6e5642d795d0cfd63866d
cjatoba <claytonjatoba@gmail.com>
2021-08-18 17:42:31 -0300

    Ajuste no nome do componente

--- app/Http/Livewire/ContactCreate.php
+++ app/Http/Livewire/ContactCreate.php
@@ -4,10 +4,10 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 
-class ContactForm extends Component
+class ContactCreate extends Component
 {
     public function render()
     {
-        return view('livewire.contact-form');
+        return view('livewire.contact-create');
     }
 }
