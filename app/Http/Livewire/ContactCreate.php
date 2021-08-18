5f2c894c75862b0ddf1563ad616844cf00d429bc
cjatoba <claytonjatoba@gmail.com>
2021-08-18 17:58:33 -0300

    Exemplo de data bind

--- app/Http/Livewire/ContactCreate.php
+++ app/Http/Livewire/ContactCreate.php
@@ -6,8 +6,10 @@ use Livewire\Component;
 
 class ContactCreate extends Component
 {
+    public $name;
+    
     public function render()
-    {
+    {        
         return view('livewire.contact-create');
     }
 }
--- resources/views/livewire/contact-create.blade.php
+++ resources/views/livewire/contact-create.blade.php
@@ -1,3 +1,6 @@
 <div>
     <h2>Hello Word!!</h2>
+
+    {{ $name }}
+    <input type="text" wire:model='name' style="border: 1px solid #f00">
 </div>
